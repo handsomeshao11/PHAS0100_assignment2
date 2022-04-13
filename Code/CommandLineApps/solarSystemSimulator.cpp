0ed4276e8921f0adac058cbc2936bd707bdd6c72
ucapjsh <ucapjsh@ucl.ac.uk>
2022-04-13 18:06:37 +0800

    add the screenshot of (3) and part of readme

--- Code/CommandLineApps/solarSystemSimulator.cpp
+++ Code/CommandLineApps/solarSystemSimulator.cpp
@@ -6,8 +6,8 @@
 #include <iomanip>
 #include <chrono>
 #include <ctime>
+#include<cstdlib>
 #include <thread>
-
 // Example, header-only library, included in project for simplicity's sake.
 #include <Eigen/Dense>
 #include <unsupported/Eigen/NonLinearOptimization>
@@ -49,11 +49,60 @@ namespace nbsim
     }
     return E_p;
   };
+
+  void helpmessage()
+  {
+    std::cout<<" -- Help message"<<"\n"
+             <<"    This is the solar system simulator.\n"
+             <<"    You can choose the timestep(unit year), and numbers of timestep(unit year) to see the beginning and the end of the bodies in the solar system.\n"
+             <<"    You can input two arguments in the terminal: ./bin/solarSystemSimulator (timestep) (number of timestep, must be double)\n"
+             <<"    Then the program print the the beginning and the end of the bodies, their energy, running time and the maximum number of processor can be used.\n"
+             <<"    For example: ./bin/solarSystemSimulator 0.000274 1.0\n\n"
+             <<std::endl;
+  };
+  
+  void errormessage()
+  {
+    std::cout<<" -- Error message"<<"\n"
+             <<"    You can input:\n"
+             <<"    ./bin/solarSystemSimulator -help\n"
+             <<"    or\n"
+             <<"    ./bin/solarSystemSimulator -h\n"
+             <<"    to get help message."<<std::endl;
+  };
 }
 
 // main function
 int main(int argc, char** argv)
 {
+  // terminal input 0 arg, like: ./bin/solarSystemSimulator
+  if (argc==1)
+  {
+    nbsim::helpmessage();
+  }
+  // terminal input 1 arg
+  else if (argc==2)
+  {
+    std::string help =argv[1];
+    if (help =="-help"||help =="-h")
+    {
+      nbsim::helpmessage();
+    }
+    else
+    {
+      std::cout<<"Error: Input arguments are not enough.\n\n";
+      nbsim::errormessage();
+    }
+  }
+  // terminal input 4 arg, like: ./bin/solarSystemSimulator 0.000274 1 16 1
+  else if (argc>=4)
+  {
+    std::cout<<"Error: Input arguments are more then needed.\n\n";
+    nbsim::errormessage();
+  }
+  // terminal input 2 or 3 arg
+  else if (argc==3)
+  {
   // start of the clock
   std::clock_t clock_start = std::clock();
   auto chrono_start = std::chrono::high_resolution_clock::now();
@@ -69,9 +118,16 @@ int main(int argc, char** argv)
     init_position=nbsim::solarSystemData[i].position;
     init_velocity=nbsim::solarSystemData[i].velocity;
     mu=nbsim::solarSystemData[i].mu;
-    std::shared_ptr<nbsim::MassiveParticle> body_ptr_i(new nbsim::MassiveParticle(name,init_position, init_velocity,mu));
+    std::shared_ptr<nbsim::MassiveParticle> body_ptr_i(new nbsim::MassiveParticle(nbsim::solarSystemData[i].name,nbsim::solarSystemData[i].position, nbsim::solarSystemData[i].velocity,nbsim::solarSystemData[i].mu));
     bodys_ptr[i]=body_ptr_i;
 	}
+  // cout the beginning of body positions
+  std::cout<<"\nStart of position:\n\n";
+  for (int i=0;i<9;i++)
+  {
+		std::cout<<bodys_ptr[i]->name<<": "<<bodys_ptr[i]->getPosition().transpose()<<std::endl;
+  }
+  std::cout<<std::endl;
   // check the body's information
 /*   for(int i=0; i<9; i++)
   {
@@ -93,10 +149,10 @@ int main(int argc, char** argv)
   double P_energy_init=nbsim::potential_energy(bodys_ptr);
   double Total_energy_init= K_energy_init+P_energy_init;
   // calculate the acceleration and update the position and velocity
-  double step_size= 0.000274;
-  omp_set_num_threads(8);
+  double step_size=atof(argv[1]);
+  int step_num= atoi(argv[2])*3650;
   #pragma omp parallel
-  for (int i=0; i<3650; i++)
+  for (int i=0; i<step_num; i++)
   {
     #pragma omp for 
 		for (int ii=0;ii<9;ii++)
@@ -110,18 +166,12 @@ int main(int argc, char** argv)
 		}	
 	}
   // cout the end of body positions
-  #pragma omp parallel for
+  std::cout<<"\nEnd of position:\n\n";
   for (int i=0;i<9;i++)
   {
-		std::cout<<name[i]<<"\n end of position:"<<bodys_ptr[i]->getPosition().transpose()<<std::endl;
+		std::cout<<bodys_ptr[i]->name<<": "<<bodys_ptr[i]->getPosition().transpose()<<std::endl;
   }
   std::cout<<std::endl;
-  // delete the memory
-/*   for (int i=0;i<9;i++)
-  {
-    delete bodys_ptr[i];
-  } */
-
   // calcualte the end of total energy
   double K_energy_end=nbsim::kinetic_energy(bodys_ptr);
   double P_energy_end=nbsim::potential_energy(bodys_ptr);
@@ -134,7 +184,6 @@ int main(int argc, char** argv)
   << " kinetic energy is "<< K_energy_end
   << ", potential energy is "<< P_energy_end
   << ", total_energy is "<< Total_energy_end<<"\n"<< std::endl;
-
   // end of the clock
   std::clock_t clock_end = std::clock();
   auto chrono_end = std::chrono::high_resolution_clock::now();
@@ -146,6 +195,97 @@ int main(int argc, char** argv)
   // get the maximum of the processor number
   int nProcessors=omp_get_max_threads();
   std::cout<<"Processor number: "<<nProcessors<<std::endl;
+  }
+
+
+
+//   // start of the clock
+//   std::clock_t clock_start = std::clock();
+//   auto chrono_start = std::chrono::high_resolution_clock::now();
+
+//   std::string name;
+//   Eigen::Vector3d init_position, init_velocity;
+//   double mu;
+//   std::shared_ptr<nbsim::MassiveParticle> bodys_ptr[9];
+//   // initialize the bodies
+//   for (int i=0; i<9; i++) 
+//   {
+//     name=nbsim::solarSystemData[i].name;
+//     init_position=nbsim::solarSystemData[i].position;
+//     init_velocity=nbsim::solarSystemData[i].velocity;
+//     mu=nbsim::solarSystemData[i].mu;
+//     std::shared_ptr<nbsim::MassiveParticle> body_ptr_i(new nbsim::MassiveParticle(name,init_position, init_velocity,mu));
+//     bodys_ptr[i]=body_ptr_i;
+// 	}
+//   // check the body's information
+// /*   for(int i=0; i<9; i++)
+//   {
+//     std::cout<<bodys_ptr[i]->name<<"\n"<<bodys_ptr[i]->getPosition()<<"\n"<<bodys_ptr[i]->getVelocity()<<"\n"<<bodys_ptr[i]->getMu()<<"\n"<<std::endl;
+//   } */
+//   // add the attractors
+//   for (int i=0; i<9; i++)
+//   {
+// 		for (int ii=0; ii<9; ii++)
+//     {
+//       if (bodys_ptr[i]->name!=bodys_ptr[ii]->name)
+//       {
+//         bodys_ptr[i]->addAttractor(bodys_ptr[ii]);
+//       }
+// 		}
+// 	}  
+//   // calculate the initial energy
+//   double K_energy_init=nbsim::kinetic_energy(bodys_ptr);
+//   double P_energy_init=nbsim::potential_energy(bodys_ptr);
+//   double Total_energy_init= K_energy_init+P_energy_init;
+//   // calculate the acceleration and update the position and velocity
+//   double step_size= 0.000274;
+//   omp_set_num_threads(8);
+//   #pragma omp parallel
+//   for (int i=0; i<3650; i++)
+//   {
+//     #pragma omp for 
+// 		for (int ii=0;ii<9;ii++)
+//     {
+// 			bodys_ptr[ii]->calculateAcceleration();
+// 		}
+//     #pragma omp for nowait
+// 		for (int iii=0;iii<9;iii++)
+//     {
+// 			bodys_ptr[iii]->integrateTimestep(step_size);
+// 		}	
+// 	}
+//   // cout the end of body positions
+//   #ifdef DEBUG
+//   for (int i=0;i<9;i++)
+//   {
+// 		std::cout<<name[i]<<"\n end of position:"<<bodys_ptr[i]->getPosition().transpose()<<std::endl;
+//   }
+//   std::cout<<std::endl;
+//   #endif
+//   // calcualte the end of total energy
+//   double K_energy_end=nbsim::kinetic_energy(bodys_ptr);
+//   double P_energy_end=nbsim::potential_energy(bodys_ptr);
+//   double Total_energy_end= K_energy_end+P_energy_end;
+//   std::cout<< "System initial energy are :"
+//   << " kinetic energy is "<<K_energy_init
+//   << ", potential energy is "<< P_energy_init
+//   << ", total_energy is "<<Total_energy_init<<"\n"<< std::endl;
+//   std::cout<< "System final energy are :"
+//   << " kinetic energy is "<< K_energy_end
+//   << ", potential energy is "<< P_energy_end
+//   << ", total_energy is "<< Total_energy_end<<"\n"<< std::endl;
+
+//   // end of the clock
+//   std::clock_t clock_end = std::clock();
+//   auto chrono_end = std::chrono::high_resolution_clock::now();
+//   std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+//             << 1000.0 * (clock_end - clock_start) / CLOCKS_PER_SEC << " ms\n"
+//             << "Wall clock time has passed: "
+//             << std::chrono::duration<double, std::milli>(chrono_end-chrono_start).count()
+//             << " ms\n"<<std::endl;
+//   // get the maximum of the processor number
+//   int nProcessors=omp_get_max_threads();
+//   std::cout<<"Processor number: "<<nProcessors<<std::endl;
 
   return 0;
 }
--- build/README.md
+++ build/README.md
@@ -25,4 +25,95 @@ and [Dr. Matt Clarkson](https://iris.ucl.ac.uk/iris/browse/profile?upi=MJCLA42).
 Build Instructions
 ------------------
 
-Build and run instructions are left as an excercise for the student. Examples of how to build using cmake were given in lectures and in the other CMake example projects.
+Open the PHAS0100Assignment2 folder.
+
+```Bash
+mkdir build
+cd build
+cmake ..
+make
+```
+
+Run Instructions
+----------------
+To run the solarSystemSimulator.cpp:
+
+Check you are in the build folder now.
+
+```Bash
+./bin/solarSystemSimulator -help
+```
+
+Then watch the print content in the terminal, and input the command as help message.
+Example:
+```Bash
+./bin/solarSystemSimulator 0.0000274 1.0
+```
+
+To run the nbsimMyFirstApp.cpp:
+/////////////////////////////////////////////////////////////
+Check you are in the build folder now.
+
+```Bash
+./bin/StillLife
+```
+
+Then watch the print content in the terminal, and input the command as help.
+Example:
+```Bash
+./bin/StillLife 4 4 8 10
+```
+////////////////////////////////////////////////////////////
+
+To run the nbsimBasicTest.cpp:
+
+Check you are in the build folder now.
+
+```Bash
+./bin/nbsimBasicTest ctest -V
+```
+
+Then watch the print content in the terminal, and you can see how many test has passed.
+
+Energy and Benchmark result:
+----------
+System initial energy are : kinetic energy is 0.187358, potential energy is -0.355517, total_energy is -0.168158
+
+There are cases with different time steps and 100 years:
+
+case 0: timestep 0.0000274  num_time 100
+
+System final energy are : kinetic energy is 0.135274, potential energy is -0.303163, total_energy is -0.16789
+CPU time used: 335077.56 ms
+Wall clock time has passed: 28189.05 ms
+
+case 1: timestep 0.0001 num_time 100
+
+System final energy are : kinetic energy is 0.149917, potential energy is -0.316279, total_energy is -0.166362
+CPU time used: 478510.46 ms
+Wall clock time has passed: 40676.55 ms
+
+case 2: timestep 0.001 num_time 100
+
+System final energy are : kinetic energy is 0.182478, potential energy is -0.324498, total_energy is -0.14202
+CPU time used: 507169.78 ms
+Wall clock time has passed: 43088.86 ms
+
+case 3: timestep 0.00274 num_time 100
+
+System final energy are : kinetic energy is 0.0876923, potential energy is -0.182417, total_energy is -0.0947247
+CPU time used: 398754.43 ms
+Wall clock time has passed: 33711.10 ms
+
+case 4: timestep 0.0274 num_time 100
+
+System final energy are : kinetic energy is 0.0439279, potential energy is -0.0566199, total_energy is -0.012692
+CPU time used: 354186.91 ms
+Wall clock time has passed: 29849.70 ms
+
+From the result, the case 0 with timestep 0.0000274, number of year 100 has the best accuracy and a great time. 
+More result you can use the solarSystemSimulator.cpp project.
+
+
+---------
+
